23f630b60f4bc947ec8c520705137eda879692b9
Andrew Novak <andrew.novak@target.com>
2019-01-21 21:47:07 -0600

    apt not yum...

--- router.go
+++ router.go
@@ -21,7 +21,7 @@ var routes = Routes{
 	//API Endpoints
 	Route{"status", "GET", "/api/alive", getStatus},
 	Route{"powerControl", "POST", "/api/power/{action}", powerAction},
-	Route{"systemUpdate", "POST", "/api/yum/{action}", yumAction},
+	Route{"systemUpdate", "POST", "/api/apt/{action}", yumAction},
 	Route{"serviceControl", "POST", "/api/service/{service}/{action}", serviceAction},
 	Route{"hdmiControl", "POST", "/api/display/{action}", displayAction},
 }
