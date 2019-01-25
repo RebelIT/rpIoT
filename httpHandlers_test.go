b59b8f36e24f36271f4926949131e0947ab5e16c
Andrew Novak <andrew.novak@target.com>
2019-01-24 23:05:03 -0600

    fixed test

--- httpHandlers_test.go
+++ httpHandlers_test.go
@@ -2,31 +2,24 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-
 func TestHttpGetStatus(t *testing.T){
 	fmt.Println("Testing GET /api/alive")
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/alive", nil)
+	r.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
-	req, err := http.NewRequest("GET", "/api/alive", nil)
-	if err != nil {
-		t.Fatal(err)
+	getStatus(w,r)
+
+	//have to trim whatespace - I don't know why this is happening yet...
+	resp := strings.Replace(w.Body.String(), "\n", "", -1)
+	expected := `{"namespace":"/api/alive","message":"alive"}`
+	if resp != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
 	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(getStatus)
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	if rr.Code != 200{
-		t.Errorf("handler returned a non-200 response")
-	}
-
 }
