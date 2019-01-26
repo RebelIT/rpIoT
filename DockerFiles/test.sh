6fdc828be882dfe457dd8c40034e317037717061
Andrew Novak <andrew.novak@target.com>
2019-01-25 23:27:40 -0600

    tests, tests, tests

--- DockerFiles/test.sh
+++ DockerFiles/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Start: testing the program"
+mkdir -p /etc/api/
+cp /go/src/github.com/rebelit/rpIoT/api_config.json /etc/api/api_config.json
 cd /go/src/github.com/rebelit/rpIoT
 go test
 echo "Done: building the program"
