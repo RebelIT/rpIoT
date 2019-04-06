24a66f567369cf03fa45934944b5de70b151991a
RebelIT <31414479+RebelIT@users.noreply.github.com>
2019-04-05 22:38:24 -0500

    shutdown endpoint

--- actions/powerControls.go
+++ actions/powerControls.go
@@ -7,21 +7,38 @@ import (
 	"time"
 )
 
+//not to self - syscall reboot magic numbers reference: https://golang.org/pkg/syscall/?GOOS=linux&GOARCH=mips64le
+
 func SystemPower(action string)(err error){
 	if err := validatePowerAction(action); err != nil{
 		return err
 	}
 
-	go reboot()
+	switch action{
+	case "reboot":
+		go reboot()
+		return nil
 
-	return nil
+	case "shutdown":
+		go shutdown()
+		return nil
+
+	default:
+		return fmt.Errorf("power action is invalid")
+	}
 }
 
 func validatePowerAction(action string) error{
-	if action != "reboot"{
-		return fmt.Errorf("power action is invalid")
+	actions := []string{
+		"reboot",
+		"shutdown",
 	}
-	return nil
+	for _, a := range actions{
+		if a == action{
+			return nil
+		}
+	}
+	return fmt.Errorf("power action is invalid")
 }
 
 func reboot(){
@@ -30,10 +47,24 @@ func reboot(){
 
 	var linuxRebootMagic1 uintptr = 0xfee1dead
 	var linuxRebootMagic2 uintptr = 672274793
-	var linuxRebootCmdRestart uintptr = 0x1234567
+	var linuxRebootCmdRestart uintptr = 0x1234567 //LINUX_REBOOT_CMD_RESTART
 
 	_,_, errno := syscall.Syscall(syscall.SYS_REBOOT, linuxRebootMagic1, linuxRebootMagic2, linuxRebootCmdRestart)
 	if errno != 0{
 		log.Printf("system reboot failed with error code %d", errno)
+	}
+}
+
+func shutdown(){
+	//best effort no return in goroutine
+	time.Sleep(time.Second * 10)
+
+	var linuxRebootMagic1 uintptr = 0xfee1dead
+	var linuxRebootMagic2 uintptr = 672274793
+	var linuxRebootCmdShutdown uintptr = 0x4321fedc //LINUX_REBOOT_CMD_POWER_OFF
+
+	_,_, errno := syscall.Syscall(syscall.SYS_REBOOT, linuxRebootMagic1, linuxRebootMagic2, linuxRebootCmdShutdown)
+	if errno != 0{
+		log.Printf("system shutdown failed with error code %d", errno)
 	}
 }
